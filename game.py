36abb6c0b52fe15c361a5b0227804d9b9bdb195c
mstepina <margarita.stepina@gmail.com>
2020-04-15 02:21:35 -0400

    dice working

--- game.py
+++ game.py
@@ -7,10 +7,10 @@ the outcome of a random coin flip or dice roll
 
 import time, random
 
-start = input("Ready to start? yes/no >>> ").lower().replace(" ", "")
-total = 0
-correct = 0
-while start == "yes":
+def coin_flip():
+    global correct
+    global total
+    global start
     print(f"Your score is {correct} out of {total}")
     guess_coin = input("Heads or tails? >>> ").lower().replace(" ", "")
     print("Let's flip it...")
@@ -29,3 +29,38 @@ while start == "yes":
         print ("You guessed wrong. Try again!")
     total += 1
     start = input("Another game? yes/no >>> ").replace(" ", "")
+
+def roll_dice():
+    global correct
+    global total
+    global start
+    print(f"Your score is {correct} out of {total}")
+    guess_dice = input("Your guess for dice? 1-6 >>> ").lower().replace(" ", "")
+    print("Let's roll it...")
+    time.sleep(1)
+    dice_res = random.randint(1, 6)
+    print(f"The result is... {dice_res}")
+    time.sleep(1)
+    if int(guess_dice) == dice_res:
+        print ("Good job! You guessed right")
+        correct += 1
+    else:
+        print ("You guessed wrong. Try again!")
+    total += 1
+    start = input("Another game? yes/no >>> ").replace(" ", "")
+
+start = input("Ready to start? yes/no >>> ").lower().replace(" ", "")
+total = 0
+correct = 0
+while start == "yes":
+    game = input("Choose a game: Coin flip or dice roll? >>> ").lower().replace(" ", "")
+    #print(game)
+    if game == "coinflip":
+        coin_flip()
+    elif game == "diceroll":
+        roll_dice()
+    else:
+        print("Something wrong. Try again")
+        start = input("Ready to start? yes/no >>> ").lower().replace(" ", "")
+
+
